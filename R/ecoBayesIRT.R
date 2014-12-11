1ef865a37768d19e35aa5a2b32452b4f8156f31e
stevencarlislewalker <scwalker@math.mcmaster.ca>
2014-12-11 10:12:34 -0500

    license

--- R/ecoBayesIRT.R
+++ R/ecoBayesIRT.R
@@ -1,3 +1,25 @@
+## ecoBayesIRT:
+## Helper functions for ecological Bayesian item response theory in R
+## 
+## Copyright (C) 2014  Steven Carlisle Walker
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License along
+## with this program; if not, write to the Free Software Foundation, Inc.,
+## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+
+
 ##' Construct an index for a Bayesian ordination (BO) mcmc object
 ##'
 ##' @param mcmc An mcmc object
